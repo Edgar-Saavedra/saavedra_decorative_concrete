f022affbd44fff4be448e0588d3efe3eab837b9a
Edgar-Saavedra <edgarsaavedraa@gmail.com>
2013-02-14 02:54:13 -0800

    custom JavaScript to make footer height expand to fill rest of viewport

--- sites/all/themes/saavedra_decorative_concrete/working_files/saavedra_decorative_concrete_HTML/js/script.js
+++ sites/all/themes/saavedra_decorative_concrete/working_files/saavedra_decorative_concrete_HTML/js/script.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
-
-
+	
+	function fillHeight(){//make the footer fill the rest of the height of the viewport
+		var topHeight = $('#container').height(); //topHeight equals the height of #continer
+		if ( topHeight > 0 ){ //if topHeight is greater than 0
+			$('footer').css({ 
+				'top' : topHeight + 'px', // add the height of #container to the top of the footer
+			});
+		}
+		else{
+			$('footer').css({
+				'position': 'relative',//else just place it below the container
+			});
+		}
+	}
+	
+	fillHeight();//call and end fillHeight
 
  }); // end ready
